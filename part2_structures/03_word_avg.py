78c4fc26c50c3f463f7c68780750cf1357265e09
Théo BIET <dev.theobiet@gmail.com>
2021-10-13 12:38:20 +0200

    🔗 | Better avg word size

--- part2_structures/03_word_avg.py
+++ part2_structures/03_word_avg.py
@@ -1,13 +1,19 @@
+import re
 phrase = "Le langage Python est placé sous une licence libre proche de la licence BSD6 et fonctionne sur la plupart des plates-formes informatiques, des smartphones aux ordinateurs centraux7, de Windows à Unix avec notamment GNU/Linux en passant par macOS, ou encore Android, iOS, et peut aussi être traduit en Java ou .NET. Il est conçu pour optimiser la productivité des programmeurs en offrant des outils de haut niveau et une syntaxe simple à utiliser."
 
-def phr_stats(phr):
-    words = phr.replace(r'[^\w\s]i', ' ').split(' ')
-    return {
-        'letter_avg': round(sum(len(word) for word in words) / len(words), 2),
-        'most_common_word': max(words, key=words.count)
-    }
+def clean_phr_to_list(phr):
+    return re.sub(r"[^\w\s]", "", phr).split()
 
-res = phr_stats(phrase)
+def letter_avg(phr):
+    words = clean_phr_to_list(phr)
+    return round(sum(len(word) for word in words) / len(words), 2)
 
-print('Average word size :', res['letter_avg'])
-print('Most used word :', res['most_common_word'])
+def most_user_word(phr): 
+    words = clean_phr_to_list(phr)
+    return max(words, key=words.count)
+
+res = letter_avg(phrase)
+res2 = most_user_word(phrase)
+
+print('Average word size :', res)
+print('Most used word :', res2)
--- part2_structures/04_sequence.py
+++ part2_structures/04_sequence.py
@@ -1,4 +1,4 @@
-l = [1,3,7,8,9,1,2,3,8]
+l = [1,3,7,8,9,1,2,3,8,1,2,3]
 s = [1, 2, 3]
 w = 'HELLO, JE TESTE'
 sw = 'JE T'
@@ -6,5 +6,10 @@ sw = 'JE T'
 def search_sequence(l, s):
     return [(i, i+len(s)) for i in range(len(list(l))) if l[i:i+len(s)] == s]
 
+def ss(l, s):
+    for i in range(len(l)):
+        if l[i:i+len(s)] == s:
+            yield i, i+len(s)
+
 print(search_sequence(l, s))
-print(search_sequence(w, sw))
+print((list(ss(l, s))))
