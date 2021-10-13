2f908f8665aa4de97db87444f68b9650577cebc9
Théo BIET <dev.theobiet@gmail.com>
2021-10-13 10:49:24 +0200

    🔒 | add assertion on matrix mult

--- part1_intro/06_matrix_mult.py
+++ part1_intro/06_matrix_mult.py
@@ -2,7 +2,9 @@
 first_matrice = [[3, 12, 4], [5, 6, 8], [1, 0, 2]]
 second_matrice = [[7, 3, 8], [11, 9, 5], [6, 8, 4]]
 
+
 def matrix_multiply(a, b):
+    assert len(a[0]) == len(b) and len(a) == len(b[0])
     result = []
     for a_col in range(len(a[0])):
         result_col = []
@@ -14,7 +16,9 @@ def matrix_multiply(a, b):
         result.append(result_col)
     return result
 
+
 def matrix_multiply_fact(a, b):
+    assert len(a[0]) == len(b) and len(a) == len(b[0])
     return [[sum(a[a_col][a_row] * b[a_row][b_col] for a_row in range(len(a))) 
         for b_col in range(len(b[0]))] 
         for a_col in range(len(a[0]))]
