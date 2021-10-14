cb84e87f044738a080b2b62510b81a1f04d8cd26
Théo BIET <dev.theobiet@gmail.com>
2021-10-14 12:58:12 +0200

    🔗 | random matrix 1.0

---
+++ part3_class/05_matrix.py
@@ -0,0 +1,34 @@
+from random import randint as ri
+from random import shuffle as sh
+
+class Matrix: 
+    def __init__(self, row, col, min=0, max=100):
+        self._row = row
+        self._col = col
+        self._matrix = [[ri(min, max) for i in range(col)] for j in range(row)]
+
+    @property
+    def row(self):
+        return self._row
+    
+    @property
+    def col(self):
+        return self._col
+    
+    @property
+    def matrix(self):
+        return self._matrix
+    
+    def __str__(self):
+        return str(self.matrix)
+
+    def shuffle_row(self):
+        sh(self.matrix)
+
+
+matrix = Matrix(9, 9)
+print(matrix)
+matrix.shuffle_row()
+print(matrix)
+
+
